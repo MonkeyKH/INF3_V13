94f19b8ed0e5226f8333b3ad201f98a0f190e8d5
Kieran <kieranh@web.de>
2024-01-15 13:20:07 +0000

    code not working

--- Versuch_13/src/main/java/versuch_13/PrimaryController.java
+++ Versuch_13/src/main/java/versuch_13/PrimaryController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
@@ -13,17 +14,20 @@ public class PrimaryController {
     private Label label;
     
     @FXML
+    private TextField textFeld;
+    
+    @FXML
     private Button button;
     
     @FXML
     private void buttonAction(ActionEvent evt)
     {
-      //model.setLabel;?
+      //model.returnALabel;?
     }
     
     public void registerBindings()
     {
-    
+      //label.textProperty().bind(textFeld.textProperty());
     }
     
 }
