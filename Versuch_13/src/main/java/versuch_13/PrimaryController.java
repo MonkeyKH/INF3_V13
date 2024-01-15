bee83a346c9a7bc4101b7eafb695b16814927a97
Kieran <kieranh@web.de>
2024-01-15 12:29:27 +0000

    working on controller

--- Versuch_13/src/main/java/versuch_13/PrimaryController.java
+++ Versuch_13/src/main/java/versuch_13/PrimaryController.java
@@ -1,12 +1,29 @@
 package versuch_13;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class PrimaryController {
 
+  
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private Label label;
+    
+    @FXML
+    private Button button;
+    
+    @FXML
+    private void buttonAction(ActionEvent evt)
+    {
+      //model.setLabel;?
     }
+    
+    public void registerBindings()
+    {
+    
+    }
+    
 }
